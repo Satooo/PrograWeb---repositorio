168db3187f3a5bb4899573860be64141068ce4cc
ximena <ximena@hotmail.com>
2022-10-02 11:14:19 -0500

    feat:about

--- ProyectoGrupalReact/src/components/About.jsx
+++ ProyectoGrupalReact/src/components/About.jsx
@@ -5,6 +5,19 @@ import "./style.css";
 import { useState } from "react";
 
 const About = ()=>{
-    
+    return <div><div className="content-about">
+     <h1>CHECKOUT OUR LATEST BUILD AND MAKE IT YOUR OWN!</h1>
+
+    <p id="content-about-subText">OUR MISSION IS TO BUILD GAMING PCS AT THE BEST PRICE WITOUT CUTTING CORNERS.</p>
+    <a href="/homepage"><button id="content-about-button">LET'S BUILD</button></a>
+   </div>
+   <div id="content-about-black">
+    <div className="LogoPC">
+        <img src="./images/PC Game.png" className="PC"/>
+
+    </div>
+    </div>
+   </div>
+
 }
 export default About;
