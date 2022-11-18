747fa34c397a3c63f23df9c991893c8affaaf979
ASato <kikopam215@gmail.com>
2022-11-18 14:09:34 -0500

    feat: estructura y data

--- backend_grupal (local) 2/scripts/backup_estructura.sql
+++ backend_grupal (local) 2/scripts/backup_estructura.sql
@@ -2,10 +2,34 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.4
--- Dumped by pg_dump version 14.4
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
 
--- Started on 2022-11-10 12:17:47
+-- Started on 2022-11-17 19:06:44 -05
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE proyectopw;
+--
+-- TOC entry 3622 (class 1262 OID 16509)
+-- Name: proyectopw; Type: DATABASE; Schema: -; Owner: grupo5
+--
+
+CREATE DATABASE proyectopw WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'C';
+
+
+ALTER DATABASE proyectopw OWNER TO grupo5;
+
+\connect proyectopw
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,194 +47,264 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 209 (class 1259 OID 24578)
--- Name: carrera; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 211 (class 1259 OID 16527)
+-- Name: Orden; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.carrera (
-    id uuid NOT NULL,
-    nombre character varying(200)
+CREATE TABLE public."Orden" (
+    "Orden_id" uuid NOT NULL,
+    "Usuario_id" uuid NOT NULL,
+    "Monto" integer NOT NULL,
+    "Direccion" character varying NOT NULL,
+    "Fecha" timestamp without time zone NOT NULL
 );
 
 
-ALTER TABLE public.carrera OWNER TO evaluaciones;
+ALTER TABLE public."Orden" OWNER TO grupo5;
 
 --
--- TOC entry 210 (class 1259 OID 24581)
--- Name: ciclo; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 214 (class 1259 OID 16558)
+-- Name: Orden_producto; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.ciclo (
-    id uuid NOT NULL,
-    nombre character varying(6) NOT NULL
+CREATE TABLE public."Orden_producto" (
+    "Orden_Producto_id" uuid NOT NULL,
+    "Orden_id" uuid NOT NULL,
+    "Producto_id" uuid
 );
 
 
-ALTER TABLE public.ciclo OWNER TO evaluaciones;
+ALTER TABLE public."Orden_producto" OWNER TO grupo5;
 
 --
--- TOC entry 211 (class 1259 OID 24584)
--- Name: curso; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 215 (class 1259 OID 16573)
+-- Name: PC_Armado; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.curso (
-    id uuid NOT NULL,
-    nombre character varying(150) NOT NULL,
-    carrera_id uuid
+CREATE TABLE public."PC_Armado" (
+    "PC_Armado_id" uuid NOT NULL,
+    "Nombre" character varying NOT NULL,
+    "Descripcion" character varying NOT NULL
 );
 
 
-ALTER TABLE public.curso OWNER TO evaluaciones;
+ALTER TABLE public."PC_Armado" OWNER TO grupo5;
 
 --
--- TOC entry 214 (class 1259 OID 32780)
--- Name: estudiante; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 216 (class 1259 OID 16600)
+-- Name: PC_Armado_Producto; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.estudiante (
-    id uuid NOT NULL,
-    username character varying NOT NULL,
-    password character varying NOT NULL
+CREATE TABLE public."PC_Armado_Producto" (
+    "PC_Armado_Producto_id" uuid NOT NULL,
+    "Producto_id" uuid NOT NULL,
+    "PC_Armado_id" uuid NOT NULL
 );
 
 
-ALTER TABLE public.estudiante OWNER TO evaluaciones;
+ALTER TABLE public."PC_Armado_Producto" OWNER TO grupo5;
 
 --
--- TOC entry 212 (class 1259 OID 24587)
--- Name: evaluacion; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 213 (class 1259 OID 16551)
+-- Name: Producto; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.evaluacion (
-    id uuid NOT NULL,
-    nombre character varying(200) NOT NULL,
-    fecha_registro timestamp without time zone,
-    curso_id uuid NOT NULL,
-    ciclo_id uuid NOT NULL
+CREATE TABLE public."Producto" (
+    "Producto_id" uuid NOT NULL,
+    "Nombre" character varying NOT NULL,
+    "Precio" integer NOT NULL,
+    "Descripcion" character varying NOT NULL,
+    "Categoria" character varying NOT NULL
 );
 
 
-ALTER TABLE public.evaluacion OWNER TO evaluaciones;
+ALTER TABLE public."Producto" OWNER TO grupo5;
 
 --
--- TOC entry 213 (class 1259 OID 32768)
--- Name: resolucion; Type: TABLE; Schema: public; Owner: evaluaciones
+-- TOC entry 210 (class 1259 OID 16517)
+-- Name: Reporte; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-CREATE TABLE public.resolucion (
-    id uuid NOT NULL,
-    estudiante_id uuid NOT NULL,
-    evaluacion_id uuid NOT NULL,
-    fecha_envio timestamp without time zone NOT NULL,
-    upvote integer,
-    url character varying
+CREATE TABLE public."Reporte" (
+    "Reporte_id" uuid NOT NULL,
+    "Usuario_id" uuid NOT NULL,
+    "Correo" character varying,
+    "Nombre" character varying,
+    "Telefono" character varying,
+    "Asunto" character varying,
+    "Descripcion" character varying
 );
 
 
-ALTER TABLE public.resolucion OWNER TO evaluaciones;
+ALTER TABLE public."Reporte" OWNER TO grupo5;
 
 --
--- TOC entry 3184 (class 2606 OID 24591)
--- Name: carrera carrera_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 212 (class 1259 OID 16539)
+-- Name: Resena; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.carrera
-    ADD CONSTRAINT carrera_pkey PRIMARY KEY (id);
+CREATE TABLE public."Resena" (
+    "Resena_id" uuid NOT NULL,
+    "Usuario_id" uuid NOT NULL,
+    "Puntaje" integer,
+    "Comentario" character varying,
+    "Video" character varying,
+    "Link" character varying,
+    "Tpo_resena" character varying
+);
 
 
---
--- TOC entry 3186 (class 2606 OID 24593)
--- Name: ciclo ciclo_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
---
-
-ALTER TABLE ONLY public.ciclo
-    ADD CONSTRAINT ciclo_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 3188 (class 2606 OID 24595)
--- Name: curso curso_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
---
-
-ALTER TABLE ONLY public.curso
-    ADD CONSTRAINT curso_pkey PRIMARY KEY (id);
-
+ALTER TABLE public."Resena" OWNER TO grupo5;
 
 --
--- TOC entry 3194 (class 2606 OID 32786)
--- Name: estudiante estudiante_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 209 (class 1259 OID 16510)
+-- Name: Usuario; Type: TABLE; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.estudiante
-    ADD CONSTRAINT estudiante_pkey PRIMARY KEY (id);
+CREATE TABLE public."Usuario" (
+    "Usuario_id" uuid NOT NULL,
+    "Nombre" character varying NOT NULL,
+    "Apellido" character varying NOT NULL,
+    "Correo" character varying NOT NULL,
+    "Contrasena" character varying NOT NULL,
+    "Direccion" character varying NOT NULL,
+    "Departamento" character varying NOT NULL,
+    "Ciudad" character varying NOT NULL,
+    "Codigo_postal" character varying NOT NULL,
+    "Telefono" character varying
+);
 
+
+ALTER TABLE public."Usuario" OWNER TO grupo5;
 
 --
--- TOC entry 3190 (class 2606 OID 24597)
--- Name: evaluacion evaluacion_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 3460 (class 2606 OID 16533)
+-- Name: Orden Orden_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.evaluacion
-    ADD CONSTRAINT evaluacion_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 3192 (class 2606 OID 32774)
--- Name: resolucion resolucion_pkey; Type: CONSTRAINT; Schema: public; Owner: evaluaciones
---
-
-ALTER TABLE ONLY public.resolucion
-    ADD CONSTRAINT resolucion_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public."Orden"
+    ADD CONSTRAINT "Orden_pkey" PRIMARY KEY ("Orden_id");
 
 
 --
--- TOC entry 3195 (class 2606 OID 24598)
--- Name: curso fk_curso_carrera; Type: FK CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 3466 (class 2606 OID 16562)
+-- Name: Orden_producto Orden_producto_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.curso
-    ADD CONSTRAINT fk_curso_carrera FOREIGN KEY (carrera_id) REFERENCES public.carrera(id);
-
-
---
--- TOC entry 3196 (class 2606 OID 24603)
--- Name: evaluacion fk_evaluacion_ciclo; Type: FK CONSTRAINT; Schema: public; Owner: evaluaciones
---
-
-ALTER TABLE ONLY public.evaluacion
-    ADD CONSTRAINT fk_evaluacion_ciclo FOREIGN KEY (ciclo_id) REFERENCES public.ciclo(id) NOT VALID;
+ALTER TABLE ONLY public."Orden_producto"
+    ADD CONSTRAINT "Orden_producto_pkey" PRIMARY KEY ("Orden_Producto_id");
 
 
 --
--- TOC entry 3197 (class 2606 OID 24608)
--- Name: evaluacion fk_evaluacion_curso; Type: FK CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 3470 (class 2606 OID 16604)
+-- Name: PC_Armado_Producto PC_Armado_Producto_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.evaluacion
-    ADD CONSTRAINT fk_evaluacion_curso FOREIGN KEY (curso_id) REFERENCES public.curso(id);
-
-
---
--- TOC entry 3199 (class 2606 OID 32787)
--- Name: resolucion fk_resolucion_estudiante; Type: FK CONSTRAINT; Schema: public; Owner: evaluaciones
---
-
-ALTER TABLE ONLY public.resolucion
-    ADD CONSTRAINT fk_resolucion_estudiante FOREIGN KEY (estudiante_id) REFERENCES public.estudiante(id) NOT VALID;
+ALTER TABLE ONLY public."PC_Armado_Producto"
+    ADD CONSTRAINT "PC_Armado_Producto_pkey" PRIMARY KEY ("PC_Armado_Producto_id");
 
 
 --
--- TOC entry 3198 (class 2606 OID 32775)
--- Name: resolucion fk_resolucion_evaluacion; Type: FK CONSTRAINT; Schema: public; Owner: evaluaciones
+-- TOC entry 3468 (class 2606 OID 16599)
+-- Name: PC_Armado PC_Armado_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
 --
 
-ALTER TABLE ONLY public.resolucion
-    ADD CONSTRAINT fk_resolucion_evaluacion FOREIGN KEY (evaluacion_id) REFERENCES public.evaluacion(id) NOT VALID;
+ALTER TABLE ONLY public."PC_Armado"
+    ADD CONSTRAINT "PC_Armado_pkey" PRIMARY KEY ("PC_Armado_id");
 
 
--- Completed on 2022-11-10 12:17:47
+--
+-- TOC entry 3464 (class 2606 OID 16557)
+-- Name: Producto Producto_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Producto"
+    ADD CONSTRAINT "Producto_pkey" PRIMARY KEY ("Producto_id");
+
+
+--
+-- TOC entry 3462 (class 2606 OID 16545)
+-- Name: Resena Resena_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Resena"
+    ADD CONSTRAINT "Resena_pkey" PRIMARY KEY ("Resena_id");
+
+
+--
+-- TOC entry 3458 (class 2606 OID 16516)
+-- Name: Usuario Usuario_pkey; Type: CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Usuario"
+    ADD CONSTRAINT "Usuario_pkey" PRIMARY KEY ("Usuario_id");
+
+
+--
+-- TOC entry 3475 (class 2606 OID 16568)
+-- Name: Orden_producto Orden_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Orden_producto"
+    ADD CONSTRAINT "Orden_fk" FOREIGN KEY ("Orden_id") REFERENCES public."Orden"("Orden_id");
+
+
+--
+-- TOC entry 3477 (class 2606 OID 16610)
+-- Name: PC_Armado_Producto PC_Armado_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."PC_Armado_Producto"
+    ADD CONSTRAINT "PC_Armado_fk" FOREIGN KEY ("PC_Armado_id") REFERENCES public."PC_Armado"("PC_Armado_id");
+
+
+--
+-- TOC entry 3474 (class 2606 OID 16563)
+-- Name: Orden_producto Producto_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Orden_producto"
+    ADD CONSTRAINT "Producto_fk" FOREIGN KEY ("Producto_id") REFERENCES public."Producto"("Producto_id");
+
+
+--
+-- TOC entry 3476 (class 2606 OID 16605)
+-- Name: PC_Armado_Producto Producto_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."PC_Armado_Producto"
+    ADD CONSTRAINT "Producto_fk" FOREIGN KEY ("Producto_id") REFERENCES public."Producto"("Producto_id");
+
+
+--
+-- TOC entry 3471 (class 2606 OID 16522)
+-- Name: Reporte Usuario_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Reporte"
+    ADD CONSTRAINT "Usuario_fk" FOREIGN KEY ("Usuario_id") REFERENCES public."Usuario"("Usuario_id");
+
+
+--
+-- TOC entry 3472 (class 2606 OID 16534)
+-- Name: Orden Usuario_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Orden"
+    ADD CONSTRAINT "Usuario_fk" FOREIGN KEY ("Usuario_id") REFERENCES public."Usuario"("Usuario_id");
+
+
+--
+-- TOC entry 3473 (class 2606 OID 16546)
+-- Name: Resena Usuario_fk; Type: FK CONSTRAINT; Schema: public; Owner: grupo5
+--
+
+ALTER TABLE ONLY public."Resena"
+    ADD CONSTRAINT "Usuario_fk" FOREIGN KEY ("Usuario_id") REFERENCES public."Usuario"("Usuario_id");
+
+
+-- Completed on 2022-11-17 19:06:44 -05
 
 --
 -- PostgreSQL database dump complete
 --
-
